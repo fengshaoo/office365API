cb970c8bdbc38b9bd7a7e8366e38cced318a61ac
fengshao <youngsfeng@outlook.com>
2025-06-24 14:26:24 +0800

    第一版上线测试v12

--- configuration/base_db_session.py
+++ configuration/base_db_session.py
@@ -11,8 +11,8 @@ class BaseDBSession:
 
     def __init__(self, database_url: str = None):
         if not BaseDBSession._engine:
-            self.database_url = database_url or Config.DATABASE_URL
-            db_url = make_url(f"mysql+pymysql://{Config.DATABASE_URL}")
+            database_url = database_url or Config.DATABASE_URL
+            db_url = make_url(f"mysql+pymysql://{database_url}")
             BaseDBSession._engine = create_engine(db_url, pool_pre_ping=True)
             BaseDBSession._SessionFactory = sessionmaker(bind=BaseDBSession._engine)
 
