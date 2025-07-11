7121772d0333c9fdfb7874db2d0ecc1f21fd46dc
fengshao <youngsfeng@outlook.com>
2025-07-12 00:24:38 +0800

    内部提交

--- utils.py
+++ utils.py
@@ -115,8 +115,8 @@ class Utils:
             }
 
             response = requests.post(telegram_url, data=payload)
-            # print_debug_info = PrintDebugInfo()
-            # print_debug_info.print_request_debug(response)
+            print_debug_info = PrintDebugInfo()
+            print_debug_info.print_request_debug(response)
         response.raise_for_status()
 
 
